a333577d9eaf0e960d9df34689a98c0e397a6df3
Lucas Nunes <161032233+lmnunes2002@users.noreply.github.com>
2024-09-28 15:21:49 +0000

    Mensagem

--- projeto/models/endereco.py
+++ projeto/models/endereco.py
@@ -1,13 +1,78 @@
 from projeto.models.enums.unidadefederativa import UnidadeFederativa
 
 class Endereco:
-    def __init__(self, logradouro: str, numero: str, complemento: str, cep: str, cidade: str, uf: UnidadeFederativa) -> None:
-        self._logradouro = logradouro
-        self._numero = numero
+    def __init__(self, logradouro: str, numero: int, complemento: str, cep: str, cidade: str, uf: UnidadeFederativa) -> None:
+        self._logradouro = self._verificar_logradouro(logradouro)
+        self._numero = self._verificar_numero(numero)
         self._complemento = complemento
         self._cep = cep
         self._cidade = cidade
         self._uf = uf
+
+    #  # Método para verificação.
+    # def _verificar_(self, valor):
+    #     """Método para verificação de """
+    #     self.__verificar__tipo_invalido(valor)
+    #     self.__verificar__vazio(valor)
+
+    #     self. = valor
+    #     return self.
+    
+    # # Método auxiliar.
+    # def __verificar__tipo_invalido(self, valor):
+    #     """Método auxiliar para verificação de tipo para """
+    #     if not isinstance(valor, str):
+    #         raise TypeError("O  deve ser um texto.")
+        
+    #  # Método auxiliar.
+    # def __verificar__vazio(self, valor):
+    #     """Método auxiliar para verificar  vazios"""
+    #     if not valor.strip():
+    #         raise TypeError("O  não pode estar vazio.")
+
+
+     # Método para verificação.
+    def _verificar_logradouro(self, valor):
+        """Método para verificação de logradouro"""
+        self.__verificar_logradouro_tipo_invalido(valor)
+        self.__verificar_logradouro_vazio(valor)
+
+        self.logradouro = valor
+        return self.logradouro
+    
+    # Método auxiliar.
+    def __verificar_logradouro_tipo_invalido(self, valor):
+        """Método auxiliar para verificação de tipo para logradouro"""
+        if not isinstance(valor, str):
+            raise TypeError("O logradouro deve ser um texto.")
+        
+     # Método auxiliar.
+    def __verificar_logradouro_vazio(self, valor):
+        """Método auxiliar para verificar logradouros vazios"""
+        if not valor.strip():
+            raise TypeError("O logradouro não pode estar vazio.")
+
+    # Método para verificação.
+    def _verificar_numero(self, valor):
+        """Método auxiliar para verificação de numero"""
+        self.__verificar_numero_tipo_invalido(valor)
+        self.__verificar_numero_vazio(valor)
+
+        self.numero = valor
+        return self.numero
+    
+    # Método auxiliar. 
+    def __verificar_numero_tipo_invalido(self, valor):
+        """Método auxiliar para verificação de tipo para numero"""
+        if not isinstance(valor, int):
+            raise TypeError("Deve constar numeração")
+        
+    # Método auxiliar.
+    def __verificar_numero_vazio(self, valor):
+        """Método auxiliar para verificação de número negativo"""
+        if valor < 0:
+            raise ValueError("O número não pode ser negativo")
+    
 
     def __str__(self) -> str:
         return (
--- projeto/tests/test_endereco.py
+++ projeto/tests/test_endereco.py
@@ -4,8 +4,44 @@ from projeto.models.enums.unidadefederativa import UnidadeFederativa
 
 @pytest.fixture
 def endereco_valido():
-    endereco = Endereco("Rua das Flores", "vinte", "Apartamento 45", "12345-678", "Salvador", UnidadeFederativa.BAHIA.nome)
+    endereco = Endereco("Rua das Flores", 20, "Apartamento 45", "12345-678", "Salvador", UnidadeFederativa.BAHIA.nome)
     return endereco
 
-def test_endereco_logradouro_valido(endereco_valido):
+def test_nome_tipo_invalido_retorna_mensagem():
+    with pytest.raises(TypeError, match="O logradouro deve ser um texto."):
+        Endereco(20, 20, "Apartamento 45", "12345-678", "Salvador", UnidadeFederativa.BAHIA.nome)
+
+def test_pessoa_nome_vazio_retorna_mensagem():
+    with pytest.raises(TypeError, match="O logradouro não pode estar vazio."):
+        Endereco("", 20, "Apartamento 45", "12345-678", "Salvador", UnidadeFederativa.BAHIA.nome)
+
+def test_numero_tipo_invalido_retorna_mensagem():
+    with pytest.raises(TypeError, match="Deve constar numeração"):
+        Endereco("Rua das Flores", "", "Apartamento 45", "12345-678", "Salvador", UnidadeFederativa.BAHIA.nome)
+
+def test_endereco_numero_negativo_retorna_mensagem():
+    with pytest.raises(ValueError, match="O número não pode ser negativo"):
+        Endereco("Rua das Flores", -20, "Apartamento 45", "12345-678", "Salvador", UnidadeFederativa.BAHIA.nome)
+
+def test_endereco_valido(endereco_valido):
     assert endereco_valido._logradouro == "Rua das Flores"
+    assert endereco_valido._numero == 20
+    assert endereco_valido._complemento == "Apartamento 45"
+    assert endereco_valido._cep == "12345-678"
+    assert endereco_valido._cidade == "Salvador"
+    assert endereco_valido._uf == UnidadeFederativa.BAHIA.nome
+
+def test_enum_membros():
+    assert UnidadeFederativa.BAHIA in UnidadeFederativa
+    assert UnidadeFederativa.SAO_PAULO in UnidadeFederativa
+    assert UnidadeFederativa.RIO_DE_JANEIRO in UnidadeFederativa
+
+def test_enum_nomes_valido():
+    assert UnidadeFederativa.BAHIA.nome == "Bahia"
+    assert UnidadeFederativa.RIO_DE_JANEIRO.nome == "Rio de Janeiro"
+    assert UnidadeFederativa.SAO_PAULO.nome == "São Paulo"
+
+def test_enum_siglas_valido():
+    assert UnidadeFederativa.BAHIA.sigla == "BA"
+    assert UnidadeFederativa.RIO_DE_JANEIRO.sigla == "RJ"
+    assert UnidadeFederativa.SAO_PAULO.sigla == "SP"
