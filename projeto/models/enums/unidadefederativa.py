164db9a88126a7cc36820cdb163f34e96e71ab98
Lucas Nunes <161032233+lmnunes2002@users.noreply.github.com>
2024-09-29 18:14:07 +0000

    Mensagem

--- projeto/models/enums/unidadefederativa.py
+++ projeto/models/enums/unidadefederativa.py
@@ -5,7 +5,7 @@ class UnidadeFederativa(Enum):
     BAHIA = "Bahia", "BA"
     SAO_PAULO = "São Paulo", "SP"
     RIO_DE_JANEIRO = "Rio de Janeiro", "RJ"
-
+    
     # Construtor.
     def __init__(self, nome, sigla) -> None:
         self.nome = nome
